b9acd3f47a14e403543f5aaf1a1d94f0616571d5
anh.pham <anh.pham081188@gmail.com>
2023-04-17 23:43:28 +0700

    changed config

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=081104062022/project4-prediction
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run anhpham-project4-prediction --image=081104062022/project4-prediction:latest --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward anhpham-project4-prediction 8080:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,11 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=anhpham/project4-prediction
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker tag anhpham/project4-prediction 081104062022/project4-prediction:latest
 # Step 3:
 # Push image to a docker repository
+docker push 081104062022/project4-prediction:latest
